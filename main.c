2b24b347000f4069a58e4be1a5c7a9f201782dce
MatteoMakovec <matteo.makovec@gmail.com>
2020-04-11 15:56:05 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+//***randomizzare le parole***
+//
+//è data una stringa di caratteri contenente parole separate dal carattere spazio
+//
+//randomizzare le parole per produrre una nuova stringa di caratteri
+//vanno riutilizzate tutte le singole parole (una sola volta ciascuna)
+//
+//[generatore di numeri casuali:
+//aggiungerò a breve un esempio in exOpSys che utilizza la funzione rand()
+//http://man7.org/linux/man-pages/man3/rand.3.html ]
+//
+//esempio:
+//
+//è data la stringa di caratteri:
+//"hello world mi chiamo pino sono date delle stringhe"
+//
+//
+//output:
+//"mi stringhe chiamo pino sono date world delle hello"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+	return 0;
+}
