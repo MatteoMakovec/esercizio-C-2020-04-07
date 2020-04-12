e5c5742e9cd447f5b787ace39b73280f6b57f2fa
MatteoMakovec <matteo.makovec@gmail.com>
2020-04-12 10:44:12 +0000

    TO DO

--- main.c
+++ main.c
@@ -22,10 +22,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
+typedef struct{
+	char *word;
+} phrase;
+
+int random_positioning(int max);
+void swap_word(phrase *phrase, int *position);
 
 int main(int argc, char *argv[]) {
-
+	phrase *line;
+	scanf("Inserire la frase da randomizzare", &line);		//ERRORE NEL SALVATAGGIO DELLA FRASE LETTA
+	int length = sizeof(phrase)/sizeof(char);
+	for(int i=0; i<length; i++){
+		swap_word(line, random_positioning(random_positioning));
+	}
 
 	return 0;
+}
+
+int random_positioning(int max){
+	time_t t = time(NULL);
+	srand(t);
+	int size = 2;
+	int *random_number = calloc(size, sizeof(int));
+	random_number[0] = rand() % max;
+	random_number[1] = rand() % max;
+	return random_number;
+}
+
+void swap_word(phrase *line, int *position) {
+	char *word = line[position[0]].word;
+	line[position[0]].word = line[position[1]].word;
+	line[position[1]].word = word;
 }
